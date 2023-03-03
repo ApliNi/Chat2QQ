783ad24cbfbed5405e55d7ea6af255e1856972b9
ApliNi <Nm70727@qq.com>
2023-03-03 23:08:30 +0800

    修复配置文件版本检查

--- src/main/java/io/github/aplini/chat2qq/utils/versionCheck.java
+++ src/main/java/io/github/aplini/chat2qq/utils/versionCheck.java
@@ -3,10 +3,12 @@ package io.github.aplini.chat2qq.utils;
 import org.bukkit.plugin.Plugin;
 
 public class versionCheck {
+
+    // 配置文件版本不同时返回 true
     public static boolean versionCheck_configVersion(Plugin plugin) {
         if(plugin.getConfig().getLong("config-version", 0) != 100){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
