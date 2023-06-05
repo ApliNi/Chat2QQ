72394f6e678d8553471fa8ecfed5a19c02a9d123
ApliNi <Nm70727@qq.com>
2023-06-05 09:40:23 +0800

    更新指令输出等待时间功能, 优化指令执行功能, 添加指令输出占位符及多行预处理功能. 更新自述文件

--- src/main/java/io/github/aplini/chat2qq/listener/onGroupMessage.java
+++ src/main/java/io/github/aplini/chat2qq/listener/onGroupMessage.java
@@ -42,48 +42,6 @@ public class onGroupMessage implements Listener {
             getServer().spigot().broadcast(formatText);
         }
 
-
-//        // 渲染消息
-//        String [] message = renderMessage(plugin, e);
-//
-//        if(! message[0].equals("") &&
-//                plugin.getConfig().getLongList("bot.bot-accounts").contains(e.getBotID()) &&
-//                plugin.getConfig().getLongList("general.group-ids").contains(e.getGroupID())){
-//
-//            // 输出到控制台
-//            if(plugin.getConfig().getBoolean("aplini.other-format-presets.message-to-log", true)){
-//                getLogger().info(message[1]);
-//            }
-//
-//            // 转换格式
-//            TextComponent formatText = new TextComponent(message[1]);
-//
-//            // 如果是长消息
-//            if(message[0].equals("lm")){
-//                message[1] = _renderMessage(plugin, e.getMessage());
-//                // 设置悬浮文本
-//                formatText.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(message[1])));
-//            }
-//
-//            // 如果是回复消息
-//            if(e.getQuoteReplyMessage() != null){
-//                // 创建回复消息的悬浮文本
-//                String replyMessage = plugin.getConfig().getString("aplini.reply-message.message", "[引用回复]")
-//                        .replace("%c_name%", ""+ cleanupName(plugin,
-//                                getNameFromCache(plugin, e.getGroupID(), e.getQuoteReplySenderID(), ""+ e.getQuoteReplySenderID()),
-//                                e.getQuoteReplySenderID()))
-//                        .replace("%qq%", ""+ e.getQuoteReplySenderID())
-//                        .replace("%_/n_%", "\n")
-//                        .replace("%message%", ""+ formatQQID(plugin, _renderMessage(plugin, e.getQuoteReplyMessage()), e.getGroupID()))
-//                        .replace("%main_message%", message[2]);
-//                // 设置悬浮文本
-//                formatText.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(replyMessage)));
-//            }
-//
-//            // 广播
-//            getServer().spigot().broadcast(formatText);
-//        }
-
     }
 
 }
