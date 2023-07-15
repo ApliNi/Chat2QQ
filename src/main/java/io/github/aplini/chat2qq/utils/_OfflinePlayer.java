22df4a8b469dcf9d5490b3095be0ff515bd683e8
ApliNi <Nm70727@qq.com>
2023-07-15 10:14:18 +0800

    修复

--- src/main/java/io/github/aplini/chat2qq/utils/_OfflinePlayer.java
+++ src/main/java/io/github/aplini/chat2qq/utils/_OfflinePlayer.java
@@ -1,14 +1,15 @@
 package io.github.aplini.chat2qq.utils;
 
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.Statistic;
+import org.bukkit.*;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.profile.PlayerProfile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
@@ -30,9 +31,75 @@ public class _OfflinePlayer implements OfflinePlayer {
         return null;
     }
 
+    /**
+     * Gets a copy of the player's profile.
+     * <p>
+     * If the player is online, the returned profile will be complete.
+     * Otherwise, only the unique id is guaranteed to be present. You can use
+     * {@link PlayerProfile#update()} to complete the returned profile.
+     *
+     * @return the player's profile
+     */
+    @NotNull
+    @Override
+    public PlayerProfile getPlayerProfile() {
+        return null;
+    }
+
     @Override
     public boolean isBanned() {
         return false;
+    }
+
+    /**
+     * Adds this user to the {@link ProfileBanList}. If a previous ban exists, this will
+     * update the entry.
+     *
+     * @param reason  reason for the ban, null indicates implementation default
+     * @param expires date for the ban's expiration (unban), or null to imply
+     *                forever
+     * @param source  source of the ban, null indicates implementation default
+     * @return the entry for the newly created ban, or the entry for the
+     * (updated) previous ban
+     */
+    @Nullable
+    @Override
+    public BanEntry<PlayerProfile> ban(@Nullable String reason, @Nullable Date expires, @Nullable String source) {
+        return null;
+    }
+
+    /**
+     * Adds this user to the {@link ProfileBanList}. If a previous ban exists, this will
+     * update the entry.
+     *
+     * @param reason  reason for the ban, null indicates implementation default
+     * @param expires instant for the ban's expiration (unban), or null to imply
+     *                forever
+     * @param source  source of the ban, null indicates implementation default
+     * @return the entry for the newly created ban, or the entry for the
+     * (updated) previous ban
+     */
+    @Nullable
+    @Override
+    public BanEntry<PlayerProfile> ban(@Nullable String reason, @Nullable Instant expires, @Nullable String source) {
+        return null;
+    }
+
+    /**
+     * Adds this user to the {@link ProfileBanList}. If a previous ban exists, this will
+     * update the entry.
+     *
+     * @param reason   reason for the ban, null indicates implementation default
+     * @param duration how long the ban last, or null to imply
+     *                 forever
+     * @param source   source of the ban, null indicates implementation default
+     * @return the entry for the newly created ban, or the entry for the
+     * (updated) previous ban
+     */
+    @Nullable
+    @Override
+    public BanEntry<PlayerProfile> ban(@Nullable String reason, @Nullable Duration duration, @Nullable String source) {
+        return null;
     }
 
     @Override
@@ -160,6 +227,17 @@ public class _OfflinePlayer implements OfflinePlayer {
     @Override
     public void setStatistic(@NotNull Statistic statistic, @NotNull EntityType entityType, int newValue) {
 
+    }
+
+    /**
+     * Gets the player's last death location.
+     *
+     * @return the last death location if it exists, otherwise null.
+     */
+    @Nullable
+    @Override
+    public Location getLastDeathLocation() {
+        return null;
     }
 
     @NotNull
